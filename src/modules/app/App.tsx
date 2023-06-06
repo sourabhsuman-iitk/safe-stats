a0e619bd962784204652a9a59f0de90b009a3587
Sourabh Suman <sourabhsumaniitk@gmail.com>
2023-06-07 04:08:15 +0530

    code cleanup and bug fix for cra

--- src/modules/app/App.tsx
+++ src/modules/app/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AppRoutes } from "../home/routes";
 import Sidebar from "../sidebar";
 import Header from "../home/components/header";
-import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
